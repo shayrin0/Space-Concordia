1d8a090f3a38452b6ed6b56c02a60f9f56c127e5
Shirin <shayrin.panahi@gmail.com>
2020-02-22 00:23:32 -0500

    different folders for pyserial and pyduino

---
+++ 1.InitiationTask/Version2/Pyserial/app.py
@@ -0,0 +1,27 @@
+import serial, time
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+ser = serial.Serial('COM5', 9600)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if "TURN_ON" in request.form:
+            ser.write('1'.encode())
+            print("TURN_ON")
+        elif "TURN_OFF" in request.form:
+            ser.write('0'.encode())
+            print("TURN_OFF")
+        elif "READ" in request.form:
+            ser.write('R'.encode())
+            while ser.inWaiting:
+                return str(ser.readline())
+    return render_template("control.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
