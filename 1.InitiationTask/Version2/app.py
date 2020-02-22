86b3d3f1df6cc5d68fadef01a21b69c5a7ef34e0
Shirin <shayrin.panahi@gmail.com>
2020-02-22 00:14:17 -0500

    serial communication with pyduino

--- 1.InitiationTask/Version2/app.py
+++ 1.InitiationTask/Version2/app.py
@@ -1,26 +1,35 @@
 
 import serial, time
 from flask import Flask, render_template, request
-
+from pyduino import Arduino
 
 app = Flask(__name__)
 
-ser = serial.Serial('COM5', 9600)
+# Pin definitions
+sensor_pin = 14
+led_pin = 13
+
+pydata = Arduino()
+time.sleep(2)
+
+# initialization
+pydata.set_pin_mode(sensor_pin,'I')
+time.sleep(0.5)
+pydata.set_pin_mode(led_pin,'O')
+time.sleep(0.5)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         if "TURN_ON" in request.form:
-            ser.write('1'.encode())
+            pydata.digital_write(led_pin,1) # Turn on Arduino's LED
             print("TURN_ON")
         elif "TURN_OFF" in request.form:
-            ser.write('0'.encode())
+            pydata.digital_write(led_pin,0) # Turn on Arduino's LED
             print("TURN_OFF")
         elif "READ" in request.form:
-            ser.write('R'.encode())
-            while ser.inWaiting:
-                return str(ser.readline())
+            return str(pydata.analog_read(14)) # Read Arduino's Sensor data
     return render_template("control.html")
 
 
