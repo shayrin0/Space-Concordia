63790080bbe75c4229f00b2235ae8f60dac43e12
Shirin <shayrin.panahi@gmail.com>
2020-02-21 23:55:16 -0500

    used pyserial

--- 1.InitiationTask/Version2/Version2.ino
+++ 1.InitiationTask/Version2/Version2.ino
@@ -1,15 +1,28 @@
 
-char operation;
+#define sensorpin A0
+int sensordata = 0;
+char incomingData;
 
 void setup() {
-    Serial.begin(9600); // Serial Port at 9600 baud
+  Serial.begin(9600);
+  pinMode(sensorpin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite (LED_BUILTIN, LOW);
 }
 
 
 void loop() {
-    // Check if characters available in the buffer
-    if (Serial.available() > 0) {
-        operation = Serial.read();
-        Serial.println("cmd = " + String(operation));
+  while (Serial.available()) {
+    incomingData = Serial.read();
+    if (incomingData == '1') {
+      digitalWrite (LED_BUILTIN, HIGH);
     }
+    else if (incomingData == '0'){
+      digitalWrite (LED_BUILTIN, LOW);
+    }
+    else if (incomingData == 'R'){
+      sensordata = analogRead(sensorpin);
+      Serial.println(sensordata);
+    }
+  }
 }
--- 1.InitiationTask/Version2/app.py
+++ 1.InitiationTask/Version2/app.py
@@ -1,44 +1,28 @@
 
 import serial, time
 from flask import Flask, render_template, request
-# from pyduino import Arduino
 
 
 app = Flask(__name__)
 
-rawdata=[]
-
-sensor_pin = 14
-led_pin = 13
-
-# pydata = Arduino()
-# time.sleep(2)
-
-# pydata.set_pin_mode(sensor_pin,'I')
-# pydata.set_pin_mode(led_pin,'O')
-# time.sleep(1)
-
-# pydata.digital_write(led_pin,0) 
+ser = serial.Serial('COM5', 9600)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        print(request.form)
-        print(type(request.form))
         if "TURN_ON" in request.form:
-            pydata.digital_write(led_pin,1)  
+            ser.write('1'.encode())
             print("TURN_ON")
         elif "TURN_OFF" in request.form:
-            pydata.digital_write(led_pin,0) 
+            ser.write('0'.encode())
             print("TURN_OFF")
         elif "READ" in request.form:
-            print(pydata.analog_read(sensor_pin))
-            data = pydata.analog_read(sensor_pin)
-            print(type(data))
+            ser.write('R'.encode())
+            while ser.inWaiting:
+                return str(ser.readline())
     return render_template("control.html")
 
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
